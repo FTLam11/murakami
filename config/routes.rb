0a7dd6cac13383e8c8574b044663ede004444f4a
Elizabeth Alexander & Frank Lam <pair+eealexander2.ftlam11@devbootcamp.com>
2016-06-03 18:54:01 -0500

    Add book controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  resources :users do #index, new, edit, show
+    resources :books #new, edit, show?, update
+  end
 
+  get '/books/search', to: 'books#search'
+
+  root to: "user#home"
 end
