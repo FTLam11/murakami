b387dc7e0e25e17e639e66e5de10a987ef8212bd
Jack Thatcher & Alex Wen <pair+jthatcher96.thatnewwen@devbootcamp.com>
2016-06-04 17:13:36 -0500

    Add some register stuff

--- rails-server/app/controllers/users_controller.rb
+++ rails-server/app/controllers/users_controller.rb
@@ -1,2 +1,32 @@
 class UsersController < ApplicationController
+
+  def index
+  end
+
+  def new
+    @user = User.new()
+  end
+
+  def create
+    print params[:user]
+    @user = User.create(email: params[:user]["email"], user_name: params[:user]["username"], hashword: params[:user]["password"])
+    if @user.save
+      session[:user_id] = @user.id
+    end
+    render json: {msg: "hello world"}.to_json
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+
+  # def user_params
+  #   params.require(:user).print(:username, :email, :password)
+  # end
+
+
+
 end
