343237e0c224e100383c9eeda8b5182ea66adf22
Elizabeth Alexander & Frank Lam <pair+eealexander2.ftlam11@devbootcamp.com>
2016-06-04 17:07:05 -0500

    Move methods to SoloReading controller

--- rails-server/app/controllers/users_controller.rb
+++ rails-server/app/controllers/users_controller.rb
@@ -1,14 +1,10 @@
 class UsersController < ApplicationController
-  def recommended_books
-    rec_criteria = recommendations(current_user.id)
-    rec_books = retrieve_rec_books(rec_criteria)
+
+  def create
+    user = User.create(params[user])
   end
 
+  def show
 
-    current_books = book_lists(current_user.id, "current")
-    favorite_books = book_lists(current_user.id, "favorite")
-    queue_books = book_lists(current_user.id, "queue")
-
-    render json: {rec_books: rec_books, current_books: current_books, favorite_books: favorite_books, queue_books: queue_books}
   end
 end
