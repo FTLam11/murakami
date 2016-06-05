e49bab123def6c033a455e9b663e5af8a8285e3f
Elizabeth Alexander & Frank Lam <pair+eealexander2.ftlam11@devbootcamp.com>
2016-06-04 19:22:26 -0500

    add validations, complete?

--- rails-server/app/models/comment.rb
+++ rails-server/app/models/comment.rb
@@ -1,4 +1,6 @@
 class Comment < ActiveRecord::Base
   belongs_to :reaction
   belongs_to :user
+
+  validates :reaction_id, :content, :user_id, presence: true
 end
--- rails-server/app/models/membership.rb
+++ rails-server/app/models/membership.rb
@@ -1,4 +1,6 @@
 class Membership < ActiveRecord::Base
   belongs_to :user
   belongs_to :group
+
+  validates :user_id, :group_id, :admin, presence: true
 end
