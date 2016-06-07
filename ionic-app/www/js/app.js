3694e2e111a53de2e27be184f64a0ae1c29d8865
Frank Lam <ryzingsun11@yahoo.com>
2016-06-07 13:57:17 -0500

    More bug fixes.

--- ionic-app/www/js/app.js
+++ ionic-app/www/js/app.js
@@ -41,7 +41,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
   // Each tab has its own nav history stack:
 
   .state('tab.dash', {
-    cache:false,
+    cache: false,
     url: '/dash',
     views: {
       'tab-dash': {
