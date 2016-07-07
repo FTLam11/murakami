31e3b5a3c1c4cc2e783b82b5fad76d4cdbe8b46a
Elizabeth Alexander <eealexander2@gmail.com>
2016-07-07 17:08:57 -0500

    Refactor controllers

--- ionic-app/www/js/controllers/bookReview.js
+++ ionic-app/www/js/controllers/bookReview.js
@@ -11,8 +11,6 @@ app.controller('BookReviewCtrl', function($scope, $http, $stateParams, $location
     }
   })
 
-
-
   $scope.submitReview = function() {
     // $scope.reviews = []
     var newReview = {content: $scope.reviewText, user_id: window.localStorage['authToken'], book_id:$stateParams.bookId, rating: $scope.rating};
